2a83db19c1514143e5b39299c3b894d75572b546
Mario Euston <116578901+MarioEuston@users.noreply.github.com>
2022-11-08 13:52:45 +0800

    Update and rename Task 1.py to 1. Task 1 - Scenairos of Flight Path Planning .py

--- 1. Task 1 - Scenairos of Flight Path Planning .py
+++ 1. Task 1 - Scenairos of Flight Path Planning .py
@@ -4,6 +4,8 @@ author: Atsushi Sakai(@Atsushi_twi)
         Nikos Kanargias (nkana@tee.gr)
 See Wikipedia article (https://en.wikipedia.org/wiki/A*_search_algorithm)
 This is the simple code for path planning class
+
+modified by: CAI Jia Liang, CHAN Pak Lam
 """
 
 
